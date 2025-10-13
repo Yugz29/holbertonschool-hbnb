a95cf632e6a63a79c9749f41c80e118d33f57e22
Yugz29 <yannduzelier@gmail.com>
2025-10-13 20:02:47 +0200

    add: class User

---
+++ part2/app/models/user.py
@@ -0,0 +1,57 @@
+from app.models.base_model import BaseModel
+
+
+class User(BaseModel):
+    def __init__(self, first_name, last_name, email, is_admin=False):
+        super().__init__()
+        self._first_name = self.string_validation(first_name, "first_name")
+        self._last_name = self.string_validation(last_name, "last_name")
+        self._email = self.email_validation(email)
+        self._is_admin = is_admin
+
+    @staticmethod
+    def string_validation(value, field_name, max_length=50):
+        """Verify first_name / last_name requirements"""
+        if not isinstance(value, str):
+            raise TypeError(f"{field_name} must be a string")
+        if not value:
+            raise ValueError(f"{field_name} is required")
+        if len(value) > max_length:
+            raise ValueError(f"{field_name} must be less than {max_length} \
+                                characters")
+        return value
+
+    @property
+    def first_name(self):
+        return self._first_name
+
+    @first_name.setter
+    def first_name(self, value):
+        self._first_name = self.string_validation(value, "first_name")
+
+    @property
+    def last_name(self):
+        return self._last_name
+
+    @last_name.setter
+    def last_name(self, value):
+        self._last_name = self.string_validation(value, "last_name")
+
+    @property
+    def email(self):
+        return self._email
+
+    @email.setter
+    def email(self, value):
+        self._email = self.email_validation(value)
+
+    @staticmethod
+    def email_validation(email):
+        """Verify email requirements"""
+        if not isinstance(email, str):
+            raise TypeError("email must be a string")
+        if email == "":
+            raise ValueError("email is required")
+        if "@" not in email:
+            raise ValueError("email must be a valid email address")
+        return email
