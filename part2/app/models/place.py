4b7b3b8687c94c4e41d7eff2b55e95c3a13df30e
Yugz29 <yannduzelier@gmail.com>
2025-10-13 20:02:12 +0200

    add: verifications

--- part2/app/models/place.py
+++ part2/app/models/place.py
@@ -10,11 +10,24 @@ class Place(BaseModel):
     def __init__(self, title, description="", price=0.0, latitude=0.0,
                  longitude=0.0, owner=None):
         super().__init__()
-        self.title = title
         self.description = description
+
+        if price < 0:
+            raise ValueError("price must be more than 0")
         self.price = price
+
+        if not (-90.0 <= latitude <= 90.0):
+            raise ValueError("latitude must be between -90.0 and 90.0")
+        if not (-180.0 <= longitude <= 180.0):
+            raise ValueError("longitude must be between -180.0 and 180.0")
         self.latitude = latitude
         self.longitude = longitude
+
+        if len(title) >= 100:
+            raise ValueError("title must be less or equal to 100 characters")
+        if not isinstance(title, str):
+            raise TypeError("title must be a string")
+        self.title = title
 
         if owner is not None and not isinstance(owner, User):
             raise TypeError("owner must be a User instance")
