d7d7a915c00b39a70c1085ada616668693b740d6
Yugz29 <yannduzelier@gmail.com>
2025-10-13 15:11:49 +0200

    add: class Amenity()

---
+++ part2/app/models/amenity.py
@@ -0,0 +1,23 @@
+from app.models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Class representif an Amenity entity"""
+
+    def __init__(self, name):
+        super().__init__()
+        if not isinstance(name, str):
+            raise TypeError("name must be a string")
+        if not name.strip():
+            raise ValueError("name cannot be empty")
+        if len(name) > 50:
+            raise ValueError("name cannot exceed 50 characters")
+        self.name = name
+
+    def to_dict(self):
+        """Return a dictionary representation of Amenity"""
+        base_dict = super().to_dict()
+        base_dict.update({
+            "name": self.name
+        })
+        return base_dict
