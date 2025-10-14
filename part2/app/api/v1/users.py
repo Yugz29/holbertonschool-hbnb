aa0b987c8c5ff926c7c754c207a94a3ba0b28606
Yugz29 <yannduzelier@gmail.com>
2025-10-14 17:18:09 +0200

    add: put method

--- part2/app/api/v1/users.py
+++ part2/app/api/v1/users.py
@@ -38,3 +38,17 @@ class UserResource(Resource):
         if not user:
             return {'error': 'User not found'}, 404
         return {'id': user.id, 'first_name': user.first_name, 'last_name': user.last_name, 'email': user.email}, 200
+
+    @api.expect(user_model, validate=True)
+    @api.response(200, 'User updated successfully')
+    @api.response(404, 'User not found')
+    def put(self, user_id):
+        """Update user details"""
+        user_data = api.payload
+        user_exist = facade.get_user(user_id)
+
+        if not user_exist:
+            return {'error': 'User not found'}, 404
+
+        updated_user = facade.update_user(user_id, user_data)
+        return {'id': updated_user.id, 'first_name': updated_user.first_name, 'last_name': updated_user.last_name, 'email': updated_user.email}, 200
