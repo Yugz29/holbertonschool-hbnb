e130e5f3a071a4d08aba3d38fb56cb456187c711
Yugz29 <yannduzelier@gmail.com>
2025-10-14 20:18:07 +0200

    add: create_amenity

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,5 +1,7 @@
 from app.persistence.repository import InMemoryRepository
 from app.models.user import User
+from app.models.amenity import Amenity
+
 
 
 class HBnBFacade:
@@ -25,8 +27,9 @@ class HBnBFacade:
         return self.user_repo.get(user_id)
 
     def create_amenity(self, amenity_data):
-        # Placeholder for logic to create an amenity
-        pass
+        amenity = Amenity(**amenity_data)
+        self.amenity_repo.add(amenity)
+        return amenity
 
     def get_amenity(self, amenity_id):
         return self.amenity_repo.get(amenity_id)
