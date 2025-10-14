f9c0dc890b0cd63e0338b5abb1d3bf6dc7a6af48
Yugz29 <yannduzelier@gmail.com>
2025-10-14 13:43:17 +0200

    add: end line

--- part2/app/api/v1/users.py
+++ part2/app/api/v1/users.py
@@ -27,4 +27,3 @@ class UserList(Resource):
 
         new_user = facade.create_user(user_data)
         return {'id': new_user.id, 'first_name': new_user.first_name, 'last_name': new_user.last_name, 'email': new_user.email}, 201
-    
