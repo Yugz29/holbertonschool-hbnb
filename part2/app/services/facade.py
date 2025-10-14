370372ec7c2bdfffea987f4a194961bf820f3145
Yugz29 <yannduzelier@gmail.com>
2025-10-14 20:03:11 +0200

    fix: update_user

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -21,4 +21,5 @@ class HBnBFacade:
         return self.user_repo.get_by_attribute('email', email)
 
     def update_user(self, user_id, user_data):
-        return self.user_repo.update(user_id, user_data)
+        self.user_repo.update(user_id, user_data)
+        return self.user_repo.get(user_id)
