9b656e61ec99ddaaf61c9d42c0bb017bcb228e19
Yugz29 <yannduzelier@gmail.com>
2025-10-15 15:11:25 +0200

    add: review methods

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,6 +1,7 @@
 from app.persistence.repository import InMemoryRepository
 from app.models.user import User
 from app.models.amenity import Amenity
+from app.models.review import Review
 from app.models.place import Place
 
 
@@ -12,6 +13,7 @@ class HBnBFacade:
         self.review_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
 
+    """user methods"""
     def create_user(self, user_data):
         user = User(**user_data)
         self.user_repo.add(user)
@@ -27,6 +29,7 @@ class HBnBFacade:
         self.user_repo.update(user_id, user_data)
         return self.user_repo.get(user_id)
 
+    """amenity methods"""
     def create_amenity(self, amenity_data):
         amenity = Amenity(**amenity_data)
         self.amenity_repo.add(amenity)
@@ -41,6 +44,28 @@ class HBnBFacade:
     def update_amenity(self, amenity_id, amenity_data):
         self.amenity_repo.update(amenity_id, amenity_data)
         return self.amenity_repo.get(amenity_id)
+
+    """review methods"""
+    def create_review(self, review_data):
+        review = Review(**review_data)
+        self.review_repo.add(review)
+        return review
+
+    def get_review(self, review_id):
+        return self.review_repo.get(review_id)
+
+    def get_all_reviews(self):
+        return self.review_repo.get_all()
+
+    def get_reviews_by_place(self, place_id):
+        all_review = self.get_all_reviews
+
+    def update_review(self, review_id, review_data):
+        self.review_repo.update(review_id, review_data)
+        return self.review_repo.get(review_id)
+
+    def delete_review(self, review_id):
+        self.review_repo.delete(review_id)
 
     def create_place(self, place_data):
         place = Place(**place_data)
