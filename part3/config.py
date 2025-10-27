710f9df92bb2e0dd7f3040393408ed4a38bfc3be
Yugz29 <yannduzelier@gmail.com>
2025-10-27 10:53:17 +0100

    add: Dev, Test and Prod config

--- part3/config.py
+++ part3/config.py
@@ -1,13 +1,15 @@
-import os
-
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
-    DEBUG = False
+    SECRET_KEY = "yannetbary"
+    TESTING = False
 
 class DevelopmentConfig(Config):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = "sqlite:///dev.db"
 
-config = {
-    'development': DevelopmentConfig,
-    'default': DevelopmentConfig
-}
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = "sqlite:///test.db"
+
+class Production(Config):
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://user:password@localhost/hbnb_db"
