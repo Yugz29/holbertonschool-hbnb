f7d6cda0bccdf712b59248768583d5e2a7e9804f
Yugz29 <yannduzelier@gmail.com>
2025-10-29 16:04:28 +0100

    refactor: rely on SQLAlchemy defaults

--- part3/app/models/base_model.py
+++ part3/app/models/base_model.py
@@ -1,19 +1,21 @@
+from app import db
 import uuid
 from datetime import datetime
 
 
-class BaseModel:
+class BaseModel(db.Model):
     """Base class for all entities with shared attributes and methods."""
+    __abstract__ = True
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
     def __init__(self):
-        """Initialize common attributes."""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        pass
 
     def save(self):
         """Update the 'updated_at' timestamp when the object is modified."""
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.utcnow()
 
     def update(self, data: dict):
         """Update object attributes from a dictionary of values."""
