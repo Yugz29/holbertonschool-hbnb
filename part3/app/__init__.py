55e7b0ed0988c8b79bd23d03479fbf7e1f415a87
Yugz29 <yannduzelier@gmail.com>
2025-10-31 20:33:16 +0100

    fix: circular import

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,15 +1,16 @@
 from flask import Flask
 from flask_restx import Api
-from app.api.v1.users import api as users_ns
-from app.api.v1.amenities import api as amenities_ns
-from app.api.v1.places import api as places_ns
-from app.api.v1.reviews import api as reviews_ns
-from app.api.v1.auth import api as auth_ns
 from app.extensions import db, bcrypt, jwt
 import config
 
 
 def create_app(config_class=config.DevelopmentConfig):
+    from app.api.v1.users import api as users_ns
+    from app.api.v1.amenities import api as amenities_ns
+    from app.api.v1.places import api as places_ns
+    from app.api.v1.reviews import api as reviews_ns
+    from app.api.v1.auth import api as auth_ns
+
     app = Flask(__name__)
     app.config.from_object(config_class)
     
