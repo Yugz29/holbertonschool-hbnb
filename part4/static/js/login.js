ac7696b7e276a61c010e8e304ca63d02691f55e3
Yugz29 <yannduzelier@gmail.com>
2025-11-15 13:54:52 +0100

    add: design

--- part4/static/js/login.js
+++ part4/static/js/login.js
@@ -1,8 +1,33 @@
-/* 
-  This is a SAMPLE FILE to get you started.
-  Please, follow the project instructions to complete the tasks.
-*/
-
 document.addEventListener('DOMContentLoaded', () => {
-    /* DO SOMETHING */
+  const loginForm = document.getElementById('login-form');
+
+  async function loginUser(email, password) {
+    const response = await fetch("http://localhost:5000/api/v1/auth/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password })
+    });
+    return response;
+  }
+
+  loginForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    try {
+      const response = await loginUser(email, password);
+
+      if (response.ok) {
+        const data = await response.json();
+        document.cookie = `token=${data.access_token}; path=/`;
+        window.location.href = "index.html";
+      } else {
+        alert("Login Failed");
+      }
+    } catch (err) {
+      console.log(err);
+    }
   });
+});
