217b3ac619a9602c190a73d38e8c503de6b0aab3
Yugz29 <yannduzelier@gmail.com>
2025-11-19 00:47:06 +0100

    add: function to get cookies, and function to check the authentification

--- part4/app/static/js/index.js
+++ part4/app/static/js/index.js
@@ -1,8 +1,27 @@
-/* 
-  This is a SAMPLE FILE to get you started.
-  Please, follow the project instructions to complete the tasks.
-*/
-
 document.addEventListener('DOMContentLoaded', () => {
-    /* DO SOMETHING */
+    checkAuthentification();
   });
+
+function getCookie(name) {
+  const pair = document.cookie
+  .split('; ')
+  .find(row => row.startsWith(name + '='));
+if (!pair) return undefined;
+return pair.split('=')[1];
+}
+
+function checkAuthentification() {
+  const token = getCookie('token');
+  const loginLink = document.getElementById('login-link');
+
+  if (!loginLink) {
+    console.warn('login-lin not found in DOM');
+  }
+
+  if (!token) {
+    if (loginLink) loginLink.style.display = 'block';
+    return;
+  }
+
+  if (loginLink) loginLink.style.display = 'none';
+}
