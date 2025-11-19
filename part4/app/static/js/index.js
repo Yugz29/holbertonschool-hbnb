5040af459e3fb3877e51fc416b086a084da608e1
Yugz29 <yannduzelier@gmail.com>
2025-11-19 19:45:43 +0100

    add: fetchPlaces, dislayPlaces and enableFiltering

--- part4/app/static/js/index.js
+++ part4/app/static/js/index.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    checkAuthentification();
+    checkAuthentication();
   });
 
 function getCookie(name) {
@@ -10,7 +10,7 @@ if (!pair) return undefined;
 return pair.split('=')[1];
 }
 
-function checkAuthentification() {
+function checkAuthentication() {
   const token = getCookie('token');
   const loginLink = document.getElementById('login-link');
 
@@ -24,4 +24,51 @@ function checkAuthentification() {
   }
 
   if (loginLink) loginLink.style.display = 'none';
+
+  fetchPlaces(token);
+}
+
+async function fetchPlaces(token) {
+  const result = await fetch('http://127.0.0.1:5000/api/v1/places', {
+    headers: {
+      "Authorization": `Bearer ${token}`
+    }
+  });
+
+  if (!result.ok) {
+    throw new Error('Unable to get Places');
+  }
+
+  const data = await result.json();
+  displayPlaces(data);
+  enableFiltering();
+}
+
+function displayPlaces(data) {
+  const list = document.getElementById('places-list');
+  list.innerHTML = '';
+
+  for (const place of data) {
+    const div = document.createElement('div');
+    div.classList.add('place-card');
+    div.dataset.price = place.price_by_night;
+
+    div.innerHTML = `
+      <h2>${place.name}</h2>
+      <p>Prix: ${place.price_by_night}€ / night</p>
+      <a href="place.html?id=${place.id}" class="details-button">View details</a>
+    `;
+    list.appendChild(div);
+  };
+}
+
+function enableFiltering() {
+  const filter = document.getElementById('price-filter');
+
+  filter.addEventListener('change', () => {
+    for (const card of cards) {
+      const price = Number.parseInt(card.dataset.price);
+      card.style.display = price <= maxPrice ? "block" : "none";
+    }
+  })
 }
