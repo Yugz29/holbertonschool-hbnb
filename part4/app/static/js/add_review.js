66c38d4838afac8e2f894a56c41f4de58c068ab6
Yugz29 <yannduzelier@gmail.com>
2025-11-23 13:02:18 +0100

    add: base

--- part4/app/static/js/add_review.js
+++ part4/app/static/js/add_review.js
@@ -1,8 +1,65 @@
-/* 
-  This is a SAMPLE FILE to get you started.
-  Please, follow the project instructions to complete the tasks.
-*/
+// Auth
+function getCookie(name) {
+  const cookies = document.cookie.split(';');
+  for (const cookie of cookies) {
+    const [key, value] = cookie.trim().split('=');
+    if (key === name) {
+      return value;
+    }
+  }
+  return null;
+}
 
+function checkAuthentication() {
+  const token = getCookie('token');
+  if (!token) {
+    window.location.href = 'index.html';
+  }
+  return token;
+}
+
+// place-id
+function getPlaceIdFromURL() {
+    const params = new URLSearchParams(window.location.search);
+    return params.get('id');
+}
+
+// DOMContentLoaded
 document.addEventListener('DOMContentLoaded', () => {
-    /* DO SOMETHING */
+  const form = document.getElementById('review-form');
+  const token = checkAuthentication();
+  const placeId = getPlaceIdFromURL();
+
+  form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const reviewText = document.getElementById('review-text').value;
+
+    const response = await submitReview(token, placeId, reviewText);
+    handleResponse(response, form);
   });
+});
+
+// POST request
+async function submitReview(token, placeId, reviewText) {
+    return fetch('http://localhost:5001/api/v1/places/' + placeId + '/reviews', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+            text: reviewText,
+            place_id: placeId
+        })
+    });
+}
+
+// Success / Error
+function handleResponse(response, form) {
+    if (response.ok) {
+        alert('Review submitted successfully!');
+        form.reset();
+    } else {
+        alert('Failed to submit review');
+    }
+}
